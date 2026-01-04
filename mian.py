e47b28a4b35c79e3474ae87832da6accf607b4dd
saymonbtw <coolmansemen@gmail.com>
2026-01-04 14:38:18 +0300

    довел до ума образование списка админов

--- mian.py
+++ mian.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv(Path(__file__).with_name(".env"))
+load_dotenv(Path(__file__).with_name("bot_info.env"))
 
 
 @dataclass
@@ -13,10 +13,10 @@ class TgBot:
 
 
 token = os.getenv("BOT_TOKEN")
-admin_id = os.getenv("ADMIN_ID")
+admin_ids = list(map(int, os.getenv("ADMIN_IDS").split(",")))
 
-if not token or not admin_id:
+if not token or not admin_ids:
     raise RuntimeError("ошибка чтения файла с данными бота")
 
-bot = TgBot(token=token, admin_ids=admin_id)
+bot = TgBot(token=token, admin_ids=admin_ids)
 print(bot)
